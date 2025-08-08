e3c7a4e657e56dfa924015aacd5d46de939ae13a
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-08 10:14:59 -0300

    Solution for zeroedMatrix

--- Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.8 Zero Matrix.xcplaygroundpage/Contents.swift
+++ Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.8 Zero Matrix.xcplaygroundpage/Contents.swift
@@ -3,7 +3,37 @@
  */
 
 func zeroedMatrix(_ matrix: [[Int]]) -> [[Int]] {
-    
+    var result = Array(
+        repeating: Array(repeating: 0, count: matrix[0].count),
+        count: matrix.count
+    )
+
+    let cols = matrix[0].count
+    let rows = matrix.count
+    var i = 0
+    var j = 0
+    var affectedRow = [Int]()
+    var affectedCol = [Int]()
+
+    for i in 0 ..< rows {
+        for j in 0 ..< cols {
+            if matrix[i][j] == 0 {
+                affectedRow.append(i)
+                affectedCol.append(j)
+                break
+            }
+        }
+    }
+
+    for i in 0 ..< result.count {
+        guard !affectedRow.contains(i) else { continue }
+        for j in 0 ..< result[i].count {
+            guard !affectedCol.contains(j) else { continue }
+            result[i][j] = matrix[i][j]
+        }
+    }
+
+    return result
 }
 
 func zeroedMatrixInPlace(_ matrix: inout [[Int]]) {
@@ -21,6 +51,12 @@ let verify = [[1, 0, 0, 4],  [0, 0, 0, 0],  [0, 0, 0, 0],  [13, 0, 0, 16]]
 assert(zeroed.elementsEqual(verify, by: ==))
 print(zeroed)
 
-var copy = matrix
-zeroedMatrixInPlace(&copy)
-assert(copy.elementsEqual(verify, by: ==))
+let matrix2 = [[1, 0, 3],
+              [4, 5, 6],
+              [7, 0, 9]]
+let zeroed2 = zeroedMatrix(matrix2)
+print(zeroed2)
+
+//var copy = matrix
+//zeroedMatrixInPlace(&copy)
+//assert(copy.elementsEqual(verify, by: ==))
