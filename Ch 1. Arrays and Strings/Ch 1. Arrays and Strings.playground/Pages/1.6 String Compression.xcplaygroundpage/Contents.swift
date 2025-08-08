69edc9de4a795a4e146b6b85904dc86ac3642020
Ignacio Cervino <ignaciocervino@gmail.com>
2025-08-08 00:01:16 -0300

    Solution for String Compression

--- Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.6 String Compression.xcplaygroundpage/Contents.swift
+++ Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.6 String Compression.xcplaygroundpage/Contents.swift
@@ -8,10 +8,32 @@
 extension String {
     
     func compressedString() -> String {
-        
+        guard let firstLetter = self.first else { return self }
+        var compressed = ""
+        var counter = 0
+        var current = firstLetter
+        compressed.append(firstLetter)
+        for letter in self {
+            if letter != current {
+                compressed.append(String(counter))
+                compressed.append(letter)
+                current = letter
+                counter = 1
+            } else {
+                counter += 1
+            }
+        }
+        compressed.append(String(counter))
+
+        if self.count > compressed.count {
+            return compressed
+        } else {
+            return self
+        }
     }
 }
 
 var s = "aabcccccaaa"
 let compressed = s.compressedString()
+print(s.compressedString())
 assert(compressed == "a2b1c5a3", "\(compressed) != a2b1c5a3")
